f6ddfa4e438996a3fe69e299c0378bbd4724aae9
Matt DeMougin <Matt.DeMougin@blackboard.com>
2017-07-01 13:50:13 -0400

    Importing

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  <h1>Hello World!</h1>,
+  document.getElementById('root')
+);
